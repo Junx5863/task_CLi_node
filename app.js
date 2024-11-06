c45b541f270434e8ca7779bebafbd5553fbf173a
Junx5863 <nicolaszamb99@gmail.com>
2024-11-05 19:50:19 -0500

    Ajustes de enlistar Tareas

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-require('colors');
+require("colors");
 
-const { inquirerMenu, pause } = require('./src/helpers/inquier');
-
-console.clear();
-
+const { guardarDb, leerDb } = require("./src/helpers/guardar_archivo");
+const { inquirerMenu, pause, leerInput } = require("./src/helpers/inquier");
+const Tareas = require("./src/models/tareas");
 
 const main = async (params) => {
-    console.log('Hello World'.green);
-    
-    let opt = '';
-    do{
-        opt = await inquirerMenu();
 
-        if(opt !== '0') await pause();
+  let opt = "";
+  const tareas = new Tareas();
+  const tareasDb = leerDb();
+
+    if (tareasDb) {
+        tareas.cargarTareasFromArray(tareasDb);
+    }
 
 
-    }while(opt !== '0');
-}
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case "1":
+        //crear Tarea
+        const desc = await leerInput("Descripción:");
+        tareas.crearTarea( desc );
+
+        break;
+      case '2':
+        tareas.listadoCompleto();
+
+        break;
+    }
+
+    guardarDb( tareas.listadoArr );
+
+    await pause();
+  } while (opt !== "0");
+};
 
 main();
--- src/helpers/inquier.js
+++ src/helpers/inquier.js
@@ -10,31 +10,31 @@ const questions = [
     choices: [
       {
         value: "1",
-        name: `${"1.".green} Crear tarea`,
+        name: `${"1.".red} Crear tarea`,
       },
       {
         value: "2",
-        name: `${"2.".green} Listar tareas`,
+        name: `${"2.".red} Listar tareas`,
       },
       {
         value: "3",
-        name: `${"3.".green} Listar tareas completadas`,
+        name: `${"3.".red} Listar tareas completadas`,
       },
       {
         value: "4",
-        name: `${"4.".green} Listar tareas pendientes`,
+        name: `${"4.".red} Listar tareas pendientes`,
       },
       {
         value: "5",
-        name: `${"5.".green} Completar tarea(s)`,
+        name: `${"5.".red} Completar tarea(s)`,
       },
       {
         value: "6",
-        name: `${"6.".green} Borrar tarea`,
+        name: `${"6.".red} Borrar tarea`,
       },
       {
         value: "0",
-        name: `${"0.".green} Salir`,
+        name: `${"0.".red} Salir`,
       },
     ],
   },
@@ -42,9 +42,9 @@ const questions = [
 
 const inquirerMenu = async () => {
   console.clear();
-  console.log("=========================".green);
-  console.log("   Seleccione una opción".green);
-  console.log("=========================\n".green);
+  console.log("===========================".green);
+  console.log("   Seleccione una opción   ".white);
+  console.log("===========================\n".green);
 
   const { option } = await inquirer.prompt(questions);
   return option;
@@ -63,7 +63,27 @@ const pause = async () => {
   await inquirer.prompt(question);
 };
 
+const leerInput = async (message) => {
+  const question = [
+    {
+      type: "input",
+      name: "desc",
+      message,
+      validate(value) {
+        if (value.length === 0) {
+          return "Por favor ingrese un valor";
+        }
+        return true;
+      },
+    },
+  ];
+
+  const { desc } = await inquirer.prompt(question);
+  return desc;
+}
+
 module.exports = {
   inquirerMenu,
   pause,
+  leerInput,
 };
